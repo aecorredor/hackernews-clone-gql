9efa8c52c5905a529146f0188c7d50ab845c16a5
Alejandro Corredor <aecorredor93@gmail.com>
2019-03-31 17:34:01 -0400

    add voting and searching, plus auto-updating the UI after changes

--- client/src/components/CreateLink.js
+++ client/src/components/CreateLink.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Mutation } from 'react-apollo';
 import gql from 'graphql-tag';
+import { FEED_QUERY } from './LinkList';
 
 const POST_MUTATION = gql`
   mutation PostMutation($description: String!, $url: String!) {
@@ -20,7 +21,7 @@ class CreateLink extends Component {
   };
 
   render() {
-    const { description, url } = this.state
+    const { description, url } = this.state;
 
     return (
       <div>
@@ -42,9 +43,18 @@ class CreateLink extends Component {
           />
         </div>
 
-        <Mutation mutation={POST_MUTATION}
+        <Mutation
+          mutation={POST_MUTATION}
           variables={{ description, url }}
           onCompleted={() => this.props.history.push('/')}
+          update={(store, { data: { post } }) => {
+            const data = store.readQuery({ query: FEED_QUERY });
+            data.feed.links.unshift(post);
+            store.writeQuery({
+              query: FEED_QUERY,
+              data,
+            });
+          }}
         >
           {postMutation => <button onClick={postMutation}>Submit</button>}
         </Mutation>
--- server/src/resolvers/Mutation.js
+++ server/src/resolvers/Mutation.js
@@ -2,13 +2,14 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { APP_SECRET, getUserId } = require('../utils')
 
-function post(parent, args, context) {
+function post(parent, args, context, info) {
+  const userId = getUserId(context)
   return context.prisma.createLink({
     url: args.url,
     description: args.description,
+    postedBy: { connect: { id: userId } },
   })
 }
-
 async function signup(parent, args, context) {
   const password = await bcrypt.hash(args.password, 10)
   const user = await context.prisma.createUser({ ...args, password })
